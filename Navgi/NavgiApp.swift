64f460ad954e2c18fc20e596ed5d2c4b6247997a
Abhishek Chikhalkar <abhichikhalkar12399@gmail.com>
2025-02-26 21:06:16 +0100

    Initial Commit

---
+++ Navgi/NavgiApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavgiApp.swift
+//  Navgi
+//
+//  Created by Abhishek Chikhalkar on 26/02/25.
+//
+
+import SwiftUI
+
+@main
+struct NavgiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
